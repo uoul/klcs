d8574bd2fabf5d6f8b5610cdf63fc0d96494201e
Lukas Neuhauser <neuhauser48@gmail.com>
2025-02-12 14:46:32 +0100

    Add mobile checkout

--- frontend/klcs/src/app/dialogs/edit-cart-dialog/edit-cart-dialog.component.ts
+++ frontend/klcs/src/app/dialogs/edit-cart-dialog/edit-cart-dialog.component.ts
@@ -3,6 +3,8 @@ import { ShoppingCartComponent } from "../../components/shopping-cart/shopping-c
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ShoppingCartService } from '../../services/shopping-cart/shopping-cart.service';
+import {ZXingScannerModule} from "@zxing/ngx-scanner";
+import { SellerApiService } from '../../services/seller-api/seller-api.service';
 
 @Component({
   selector: 'klcs-edit-cart-dialog',
@@ -10,6 +12,7 @@ import { ShoppingCartService } from '../../services/shopping-cart/shopping-cart.
     CommonModule,
     FormsModule,
     ShoppingCartComponent,
+    ZXingScannerModule,
   ],
   templateUrl: './edit-cart-dialog.component.html',
   styleUrl: './edit-cart-dialog.component.css'
@@ -20,18 +23,53 @@ export class EditCartDialogComponent {
 
   paymentMethod: number = 0;
   accountId: string = "";
+  description: string = "";
+  scannerActive: boolean = false;
 
   constructor(
     protected shoppingCart: ShoppingCartService,
+    private sellerApi: SellerApiService,
   ){}
 
-  checkOut() {
-    console.log("checkout")
+  checkout() {
+    if(this.paymentMethod == 2){
+      const sub = this.sellerApi.checkoutCard(this.accountId, this.description).subscribe({
+        next: val => {
+          console.log(JSON.stringify(val))
+          this.shoppingCart.clearCart()
+        },
+        error: err => console.error(err),
+        complete: () => sub.unsubscribe()
+      })
+    }
+    else if(this.paymentMethod == 1) {
+      const sub = this.sellerApi.checkoutCash(this.description).subscribe({
+        next: val => {
+          console.log(JSON.stringify(val))
+          this.shoppingCart.clearCart()
+        },
+        error: err => console.error(err),
+        complete: () => sub.unsubscribe()
+      })
+    }
   }
 
   _dialogClosed(){
     this.paymentMethod = 0;
     this.accountId = "";
+    this.description = "";
+    this.scannerActive = false;
     this.dialogClosed.emit();
   }
+
+  onScanSuccess(data: string) {
+    this.accountId = data;
+    this.scannerActive = false;
+  }
+
+  onScanError(error: Error){
+    console.error(error)
+    this.scannerActive = false;
+  }
+
 }
